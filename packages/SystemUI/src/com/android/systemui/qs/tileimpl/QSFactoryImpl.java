e45b1b2207ea4ff3b4d105e4bfc26b54779f0161
Jon West <electrikjesus@gmail.com>
2020-04-25 18:33:08 -0400

    Revert "QS: introduce ADB tile (1/2)"
    This reverts commit bf906616938dab02f165f532c7b8ed418af44cda.
    
    Change-Id: Ia1a720f1ca9ed747309955960742f7fc73d65e18
    
    Conflicts:
    	packages/SystemUI/res/values/bliss_config.xml

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -28,7 +28,6 @@ import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.QSTileHost;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
-import com.android.systemui.qs.tiles.AdbTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.AODTile;
@@ -106,7 +105,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<NfcTile> mNfcTileProvider;
     private final Provider<GarbageMonitor.MemoryTile> mMemoryTileProvider;
     private final Provider<UiModeNightTile> mUiModeNightTileProvider;
-    private final Provider<AdbTile> mAdbTileProvider;
     private final Provider<AdbOverNetworkTile> mAdbOverNetworkTileProvider;
     private final Provider<AmbientDisplayTile> mAmbientDisplayTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
@@ -158,7 +156,6 @@ public class QSFactoryImpl implements QSFactory {
             Provider<NfcTile> nfcTileProvider,
             Provider<GarbageMonitor.MemoryTile> memoryTileProvider,
             Provider<UiModeNightTile> uiModeNightTileProvider,
-            Provider<AdbTile> adbTileProvider,
             Provider<AdbOverNetworkTile> adbOverNetworkTileProvider,
             Provider<AmbientDisplayTile> ambientDisplayTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
@@ -206,7 +203,6 @@ public class QSFactoryImpl implements QSFactory {
         mNfcTileProvider = nfcTileProvider;
         mMemoryTileProvider = memoryTileProvider;
         mUiModeNightTileProvider = uiModeNightTileProvider;
-        mAdbTileProvider = adbTileProvider;
         mAdbOverNetworkTileProvider = adbOverNetworkTileProvider;
         mAmbientDisplayTileProvider = ambientDisplayTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
@@ -291,8 +287,6 @@ public class QSFactoryImpl implements QSFactory {
             // Custom tiles.
             case "adb_network":
                 return mAdbOverNetworkTileProvider.get();
-	    case "adb":
-                return mAdbTileProvider.get();
             case "ambient_display":
                 return mAmbientDisplayTileProvider.get();
             case "caffeine":
