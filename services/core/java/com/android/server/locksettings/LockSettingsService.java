29e1cdc26952c32b5899b2791cdcf9e92d868809
Jon West <electrikjesus@gmail.com>
2020-04-29 11:06:01 -0400

    Revert "Fix Build Credentials refactored to use byte[]"
    This reverts commit a1a738cda1f35d6fc02903654f20d6214b8a7fdc.

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1847,9 +1847,7 @@ public class LockSettingsService extends ILockSettings.Stub {
                                         CHALLENGE_NONE, 0, userId, progressCallback);
         if ((response.getResponseCode() == VerifyCredentialResponse.RESPONSE_OK) &&
                                            (userId == UserHandle.USER_OWNER)) {
-                //TODO(b/127810705): Update to credentials to use byte[]
-                String credentialString = credential == null ? null : new String(credential);
-                retainPassword(credentialString);
+                retainPassword(credential);
         }
         return response;
     }
