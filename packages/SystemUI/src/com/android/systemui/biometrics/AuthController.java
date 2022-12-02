f56e8bef4169e9b55dd604f8e593e8b3468b4ed4
hmtheboy154 <buingoc67@gmail.com>
2022-12-03 03:29:47 +0700

    Revert "SystemUI: AuthController: Fix ripple animation for side fp devices"
    This reverts commit 4c8e4526c83aca0f74d541166a0f620ad0e05cb5.

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
@@ -523,12 +523,7 @@ public class AuthController extends SystemUI implements CommandQueue.Callbacks,
     }
 
     private void updateFingerprintLocation() {
-        int xLocation;
-        if (mFingerprintManager != null && mFingerprintManager.isPowerbuttonFps()) {
-            xLocation = DisplayUtils.getWidth(mContext);
-        } else {
-            xLocation = DisplayUtils.getWidth(mContext) / 2;
-        }
+        int xLocation = DisplayUtils.getWidth(mContext) / 2;
         try {
             xLocation = mContext.getResources().getDimensionPixelSize(
                     com.android.systemui.R.dimen
