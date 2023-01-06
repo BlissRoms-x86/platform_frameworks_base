a1ab51bcd583a80e274fec7b0701002b087121db
hmtheboy154 <buingoc67@gmail.com>
2023-01-06 15:06:59 -0500

    Revert "Adding the missing part on BoringDroid patch"
    This reverts commit d3974cb8de42c15edd237fcbedf9d9488146ae97.

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -1610,19 +1610,7 @@ class ActivityStarter {
             boolean restrictedBgActivity, NeededUriGrants intentGrants) {
         setInitialState(r, options, inTask, doResume, startFlags, sourceRecord, voiceSession,
                 voiceInteractor, restrictedBgActivity);
-        // region @boringdroid
-        if (mOptions == null) {
-            mOptions = ActivityOptions.makeBasic();
-        }
-        if (mOptions.getLaunchWindowingMode() == WINDOWING_MODE_UNDEFINED) {
-            mOptions.setLaunchWindowingMode(
-                    BoringdroidManager.getPackageWindowingMode(
-                            WindowManagerService.getWMSContext(),
-                            mStartActivity.info.packageName
-                    )
-            );
-        }
-        // endregion
+
         computeLaunchingTaskFlags();
 
         computeSourceStack();
