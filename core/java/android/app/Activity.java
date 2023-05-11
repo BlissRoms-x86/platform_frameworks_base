e5afc35af2bbdc364d58dc1233f002d3fa1034bc
hmtheboy154 <buingoc67@gmail.com>
2023-05-11 12:17:40 -0400

    Revert "FakeClickAsTouch : replace the old 4098 to SOURCE_TOUCHSCREEN"
    This reverts commit c67070175c8f76f907f7d52f636a1de27771652a.

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -106,7 +106,6 @@ import android.view.DragEvent;
 import android.view.KeyEvent;
 import android.view.KeyboardShortcutGroup;
 import android.view.KeyboardShortcutInfo;
-import android.view.InputDevice;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -4223,7 +4222,7 @@ public class Activity extends ContextThemeWrapper
     public boolean dispatchTouchEvent(MotionEvent ev) {
         int action = ev.getAction();
         if((ActivityManager.getFakeClickAsTouch() == 1) && (action == MotionEvent.ACTION_MOVE || action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_UP)) {
-            ev.setSource(InputDevice.SOURCE_TOUCHSCREEN);
+            ev.setSource(4098);
         } else {
         }
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
