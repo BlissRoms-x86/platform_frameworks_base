081aff498ac135e035650052dc86811d787e5645
hmtheboy154 <buingoc67@gmail.com>
2023-08-23 14:20:14 -0400

    Bypass restrictions on low RAM devices or Go builds [3/3]
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6485,6 +6485,11 @@ public final class Settings {
         public static final String FORCE_MOUSE_AS_TOUCH = "force_mouse_as_touch";
 
         /**
+         * @hide
+         */
+        public static final String ALERT_WINDOW_BYPASS_LOW_RAM = "alert_window_bypass_low_ram";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
